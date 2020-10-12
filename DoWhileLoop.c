b9234f9a046a174aea183b8435775f1d6a21a58f
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-13 02:19:49 +0530

    Program to demonstrate DO-WHILE LOOP in c

---
+++ DoWhileLoop.c
@@ -0,0 +1,20 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate DO-WHILE LOOP in c.
+Author  : Chpaone09®
+Date    : Oct 12, 2020
+
+************************************************************
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+    int i=1;
+    printf("\n\n\t Demonstration of DO-WHILE LOOP in C >> ");
+    do{
+        printf(" %d", i++);
+    }while(i<=5);
+}
